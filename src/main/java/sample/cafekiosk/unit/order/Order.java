78d0c15ad6ec204502a9256f4fb1161fd5df4671
mirea70 <deae670@daum.net>
2025-11-20 18:10:53 +0900

    Business Layer 테스트

--- src/main/java/sample/cafekiosk/unit/order/Order.java
+++ src/main/java/sample/cafekiosk/unit/order/Order.java
@@ -2,6 +2,7 @@ package sample.cafekiosk.unit.order;
 
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import sample.cafekiosk.spring.domain.product.Product;
 import sample.cafekiosk.unit.beverage.Beverage;
 
 import java.time.LocalDateTime;
@@ -10,6 +11,8 @@ import java.util.List;
 @Getter
 @RequiredArgsConstructor
 public class Order {
+
     private final LocalDateTime orderDateTime;
     private final List<Beverage> beverages;
+
 }
